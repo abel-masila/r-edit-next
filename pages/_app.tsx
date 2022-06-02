ac3a239f643b82b15995e2ab8e4fb0ed1349a464
Abel Masila <abel@x2ai.com>
2022-06-03 02:14:45 +0300

    Added header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import { SessionProvider } from 'next-auth/react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Header from '../components/Header'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <div className="h-screen overflow-y-scroll bg-slate-200">
+        <Header />
+        <Component {...pageProps} />
+      </div>
     </SessionProvider>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,6 @@ const Home: NextPage = () => {
       <Head>
         <title>Reddit on Web 3.0</title>
       </Head>
-      <Header />
     </div>
   )
 }
